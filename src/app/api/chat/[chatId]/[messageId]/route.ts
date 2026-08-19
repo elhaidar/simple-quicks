3c94b2e4a79535ac823cf4e14d51775662b6f62f
Haidar <elhaidar45@gmail.com>
2026-08-19 11:34:27 +0700

    fix: build error

--- src/app/api/chat/[chatId]/[messageId]/route.ts
+++ src/app/api/chat/[chatId]/[messageId]/route.ts
@@ -2,10 +2,19 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { chatId: string; messageId: string } }
+  {
+    params,
+  }: {
+    params: Promise<{
+      chatId: string;
+      messageId: string;
+    }>;
+  },
 ) {
+  const { chatId, messageId } = await params;
+
   return NextResponse.json({
-    data: { chatId: params.chatId, messageId: params.messageId },
+    data: { chatId, messageId },
     message: "Message deleted",
     success: true,
   });
@@ -13,6 +22,7 @@ export async function DELETE(
 
 export async function PATCH(req: NextRequest) {
   const res = await req.json();
+
   return NextResponse.json({
     data: res,
     success: true,
--- src/app/api/todo/[id]/route.ts
+++ src/app/api/todo/[id]/route.ts
@@ -4,16 +4,18 @@ export async function PATCH(req: NextRequest) {
   const res = await req.json();
   return NextResponse.json(
     { data: res, success: true, message: "Task updated successfully" },
-    { status: 200 }
+    { status: 200 },
   );
 }
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> },
 ) {
+  const { id } = await params;
+
   return NextResponse.json({
-    id: params.id,
+    id,
     success: true,
     message: "Task deleted",
   });
